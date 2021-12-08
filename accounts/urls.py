650457e4c2562de0f8d04d3350d5ca26b4a623ad
Keshari Nandan <keshari.nandan2@gmail.com>
2021-12-08 23:14:10 +0530

    Added forgot password and reset password options

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('logout/', views.logout, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+
+    path('forgotPassword/', views.forgot_password, name='forgot_password'),
+    path('forgotPassword_validate/', views.forgot_password_validate,
+         name='forgot_password_validate'),
+    path('resetPassword/', views.reset_password, name='reset_password'),
 ]
